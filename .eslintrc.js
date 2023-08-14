be11c83f4969b92618e49d768ce5bc868ecb3397
ics-kitagawa <kitagawa@ics-web.jp>
2023-08-14 19:02:06 +0900

    リンターの導入

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+      rules: {
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "semi-spacing": ["error", { after: true, before: false }],
+        "semi-style": ["error", "last"],
+        "no-extra-semi": "error",
+        "no-unexpected-multiline": "error",
+        "no-unreachable": "error",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
