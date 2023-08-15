aea569d52e4f8184913eb0a4cefc896a01f5df83
ics-kitagawa <kitagawa@ics-web.jp>
2023-08-15 20:10:17 +0900

    モデルのディレクトリを変更

--- src/scripts/three/loadModel.js
+++ src/scripts/three/loadModel.js
@@ -3,7 +3,7 @@ import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 export const loadModel = () => {
   const gltfLoader = new GLTFLoader();
   return new Promise((resolve) => {
-    gltfLoader.load("/models/rabbit.glb", (gltf) => {
+    gltfLoader.load("assets/models/rabbit.glb", (gltf) => {
       resolve(gltf.scene);
     });
   });
